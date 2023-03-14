b08bf82e671b874edc4b349aa5cadcbc75f2077c
janushreedurai <janushreedurai@gmail.com>
2023-03-14 17:30:19 +0530

    angular basic

--- angularbasic/src/app/app.component.ts
+++ angularbasic/src/app/app.component.ts
@@ -2,8 +2,12 @@ import { Component } from '@angular/core';
 
  @Component({
    selector: 'app-root',
-  templateUrl: './app.component.html',
-   styleUrls: ['./app.component.css']
+  //  template: '<h1>Template in ts file',
+      // template:`<h1>Template in ts file</h1>
+      //           <p>Paragraph is written in app component ts file</p>`,
+    templateUrl: './app.component.html',
+      styleUrls: ['./app.component.css']
+      // styles:['p{color:green}']
 })
 export class AppComponent {
   title = 'Angular Basic';
