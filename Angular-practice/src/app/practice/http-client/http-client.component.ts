287e197ecb9b7be9681f0fa41daa2a73719330db
JanuDurai <janu.nagarajan@aspiresys.com>
2024-02-23 12:40:18 +0530

    practice

--- Angular-practice/src/app/practice/http-client/http-client.component.ts
+++ Angular-practice/src/app/practice/http-client/http-client.component.ts
@@ -1,30 +1,57 @@
 import { Component, OnInit, inject } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 
 @Component({
   selector: 'app-http-client',
   templateUrl: './http-client.component.html',
   styleUrls: ['./http-client.component.css'],
 })
-export class HttpClientComponent implements OnInit{
-
-  constructor(private http:HttpClient){}
-  postId;
+export class HttpClientComponent implements OnInit {
+  getData;
+  param = new HttpParams();
+  params;
+  username = 'janu';
+  url = '../assets/data.json';
  
+
+  constructor(private httpReq: HttpClient) {}
+
   ngOnInit(): void {
-    
-    //  const body={property: "updated property"};
-        
-    //    this.http.get("https://run.mocky.io/v3/6f239562-1ad9-4de8-99b8-3cff7fc4b644").subscribe(data => console.log(data));
-
-
-    //    this.http.post<any> ('https://jsonplaceholder.typicode.com/posts/1', body).subscribe(data=> console.log(data));
-
-    //    this.http.put<any> ('https://jsonplaceholder.typicode.com/posts/1', body).subscribe(data=> this.postId=data.id);
-
-    //     this.http.delete('https://jsonplaceholder.typicode.com/posts/1').subscribe(() => console.log('Delete successful'));
-    
+    this.httpReq.get(this.url).subscribe((data) => console.log(data));
   }
 
-}
+  setParam() {
+    const des = 'Jsonserverdata';
+    this.param = this.param.set('pageno', 1);
+    this.param = this.param.set('description', des);
+    this.param = this.param.set('pageno', 2);
+    this.param = this.param.set('sample', 'dummy');
+    this.param = this.param.append('pageno', 4);
+    //    this.param = this.param.append('pageno',5);
+    console.log(this.param.toString());
+    if (this.param.has('page')) console.log('param has page property');
+    else console.log('param does not have page property');
+    console.log(`get a first value of property:`, this.param.get('pageno'));
+    console.log(`get a all value of property:`, this.param.getAll('pageno'));
+    console.log(`keys in params`, this.param.keys());
+    this.param = this.param.delete('pageno', '4');
+    this.param = this.param.delete('pageno');
+    this.param = this.param.delete(' ');
+    console.log(`param`, this.param.toString());
+    console.log(
+      '.............',
+      this.httpReq.get(this.url).subscribe((data) => console.log(data))
+    );
+    this.params = new HttpParams({
+      fromString: 'Pageno=' + 1 + '&Title=' + 'Queryparams',
+    });
+    console.log(`Query params: `, this.params.toString());
+    this.params = new HttpParams({
+      fromObject: { pageNo: 1, title: 'stringparams' },
+    });
+    console.log(`Query params: `, this.params.toString());
+  }
 
+
+
+}
