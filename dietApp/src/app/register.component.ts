d2eaf0efacc58b8413f86106f6c8b9357830204d
JanuDurai <janu.nagarajan@aspiresys.com>
2024-02-27 10:47:15 +0530

    practice

--- dietApp/src/app/register.component.ts
+++ dietApp/src/app/register.component.ts
@@ -1,4 +1,6 @@
+import { formatCurrency } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormGroup,FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -6,5 +8,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./register.component.scss']
 })
 export class RegisterComponent {
+  
+  Data= new FormGroup({
+    userfirstname:new FormControl(''),
+    userlastname:  new FormControl(''),
+    userage: new FormControl(''),
+    userheight: new FormControl(''),
+    userweight:new FormControl(''),
+    usertargetweight: new FormControl(''),
+    userchoice: new FormControl(''),
+    userdaystoachieve: new FormControl(''),
+    userusername: new FormControl(''),
+    userpassword: new FormControl(''),
+    userconfirmpassword: new FormControl('')
+ });
+
+DataTransfer(){
+   
+}
 
 }
--- dietApp/src/app/shared-routing.module.ts
+++ dietApp/src/app/shared-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home.component';
+import { LoginComponent } from './login.component';
+import { RegisterComponent } from './register.component';
+import { ProfileComponent } from './profile.component';
 
-const routes: Routes = [];
+const routes: Routes = [{path:"Home", component:HomeComponent},
+{path:"Login", component:LoginComponent},
+{path:"Register", component:RegisterComponent},
+{path:"Profile",component:ProfileComponent}];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
